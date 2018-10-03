c105b6f30e81aa1ea2380259a31773324484ed54
madisonbynum <43451484+madisonbynum@users.noreply.github.com>
2018-10-03 00:02:11 -0400

    Add files via upload
    Finished Project

--- Rick & Morty Project/homepage.js
+++ Rick & Morty Project/homepage.js
@@ -1,3 +1,5 @@
+//variable state, states what information
+// I will get from the api
 let state = {
     id: '',
     name: '',
@@ -8,9 +10,20 @@ let state = {
     image: '',
 };
 
+
+//This is my random page generator function
+//I realize now that this is unnessesary because I can use
+// .value and use a temperate literal to append the value
+// to the end of the apiURl, but I didn't know how to do that
+//when I made my randomizer
 let randApiUrlPage = function () {
+    //this is my random page expression. It generates a random number
+    //between 1 and 25.
     switch (Math.ceil(Math.random() * 25)){
 
+        //default case leads to a character on page 1
+        //case 2-25 lands on the corresponding page.
+        //ex 2 = page 2 ...
         case 2: apiUrl= ('https://rickandmortyapi.com/api/character/?page=2');
         return apiUrl;
         case 3: apiUrl= ('https://rickandmortyapi.com/api/character/?page=3');
@@ -65,28 +78,54 @@ let randApiUrlPage = function () {
     }
 }
 
+//This is my async function that calls the api using fetch
+//This api call only calls one random character to load
+//into my character-stat-card 1 on top left of the html page.
 let getCharacterUsingAsync1 = async function(){
+    
+    //Call of the random page
     randApiUrlPage();
 
+    //try/catch for the api Url
     try{
+        //fetching data with the api Url generated with randApiUrlPage
         const response = await fetch(apiUrl);
+        //awaiting the api data and storing the json request in js variable data
         const data = await response.json();
+        // the data from json is storred in an array
+        //index uses a formula to choose a random character from the array
+        //organized by id
         let index = Math.floor(Math.random()*data.results.length);
+
+        //some characters are not appropriate for the workplace so I've filtered these ID's
+        //its about 21 different ids
         const excludeArray = [98, 99, 140, 151, 153, 157, 192, 238, 244, 247, 271, 275, 309, 323, 342, 436, 459, 30, 41, 55, 97];
+        //My while loop to continue to choose a random ID until
+        //a workplace approved ID is chosen
         while (excludeArray.includes(data.results[index].id)){
                 index = Math.floor(Math.random()*data.results.length);
                 console.log(index);
             }
+
+        //after the loop, the character is stored in the state const from line 1
         state = data.results[index];
+        //the state is used in updateCard1 function
         updateCard1();
+
+        //in case there is an error
     } catch (err) {
         console.log('Error occured');
         console.error(err.stack);
     }
 }
 
+//This function uses getCharacterUsingAsync1 to update my first character
+//card in the upper left of the html page
+//1 in the id denotes the 1st character card
 let updateCard1 = function() {
 
+    //all of these individual statements connects the js variable
+    //to the ID in the HTML document
     const id = document.getElementById('character-id1');
     const name = document.getElementById('character-name1');
     const status = document.getElementById('character-status1');
@@ -95,6 +134,8 @@ let updateCard1 = function() {
     const origin = document.getElementById('character-origin1');
     const img = document.querySelector('#character-img1>img');
 
+    //each line changes the inner text of the ID's from above into
+    //the data from the api
     id.innerText = state.id;
     name.innerText = state.name;
     status.innerText = state.status;
@@ -104,28 +145,55 @@ let updateCard1 = function() {
     img.src = state.image;
 }
 
+//This is my async function that calls the api using fetch
+//This api call only calls one random character to load
+//into my character-stat-card 2 on top right of the html page.
 let getCharacterUsingAsync2 = async function(){
+
+    //Call of the random page
     randApiUrlPage();
+
+    //try/catch for the api Url
     try{
+        //fetching data with the api Url generated with randApiUrlPage
         const response = await fetch(apiUrl);
+        //awaiting the api data and storing the json request in js variable data
         const data = await response.json();
+         // the data from json is storred in an array
+        //index uses a formula to choose a random character from the array
+        //organized by id
         let index = Math.floor(Math.random()*data.results.length);
+
+         //some characters are not appropriate for the workplace so I've filtered these ID's
+        //its about 21 different ids
         const excludeArray = [98, 99, 140, 151, 153, 157, 192, 238, 244, 247, 271, 275, 309, 323, 342, 436, 459, 30, 41, 55, 97];
+         //My while loop to continue to choose a random ID until
+        //a workplace approved ID is chosen
         while (excludeArray.includes(data.results[index].id)){
             index = Math.floor(Math.random()*data.results.length);
             console.log(index);
         }
+
+        //after the loop, the character is stored in the state const from line 1
         state = data.results[index];
+        //the state is used in updateCard2 function
         updateCard2();
+
+         //in case there is an error
     } catch (err) {
         console.log('Error occured');
         console.error(err.stack);
     }
 }
 
+//This function uses getCharacterUsingAsync2 to update my second character
+//card in the upper right of the html page
+//2 in the id denotes the 2nd character card
 let updateCard2 = function() {
     console.log(state);
 
+    //all of these individual statements connects the js variable
+    //to the ID in the HTML document
     const id = document.getElementById('character-id2');
     const name = document.getElementById('character-name2');
     const status = document.getElementById('character-status2');
@@ -134,6 +202,8 @@ let updateCard2 = function() {
     const origin = document.getElementById('character-origin2');
     const img = document.querySelector('#character-img2>img');
 
+    //each line changes the inner text of the ID's from above into
+    //the data from the api
     id.innerText = state.id;
     name.innerText = state.name;
     status.innerText = state.status;
@@ -143,28 +213,55 @@ let updateCard2 = function() {
     img.src = state.image;
 }
 
+//This is my async function that calls the api using fetch
+//This api call only calls one random character to load
+//into my character-stat-card 1 on bottom left of the html page.
 let getCharacterUsingAsync3 = async function(){
+
+    //Call of the random page
     randApiUrlPage();
+
+    //try/catch for the api Url
     try{
+        //fetching data with the api Url generated with randApiUrlPage
         const response = await fetch(apiUrl);
+        //awaiting the api data and storing the json request in js variable data
         const data = await response.json();
+         // the data from json is storred in an array
+        //index uses a formula to choose a random character from the array
+        //organized by id
         let index = Math.floor(Math.random()*data.results.length);
+
+         //some characters are not appropriate for the workplace so I've filtered these ID's
+        //its about 21 different ids
         const excludeArray = [98, 99, 140, 151, 153, 157, 192, 238, 244, 247, 271, 275, 309, 323, 342, 436, 459, 30, 41, 55, 97];
+         //My while loop to continue to choose a random ID until
+        //a workplace approved ID is chosen
         while (excludeArray.includes(data.results[index].id)){
             index = Math.floor(Math.random()*data.results.length);
             console.log(index);
         }
+
+        //after the loop, the character is stored in the state const from line 1
         state = data.results[index];
+        //the state is used in updateCard3 function
         updateCard3();
+
+        //in case there is an error
     } catch (err) {
         console.log('Error occured');
         console.error(err.stack);
     }
 }
 
+//This function uses getCharacterUsingAsync3 to update my first character
+//card in the bottom left of the html page
+//3 in the id denotes the 3rd character card
 let updateCard3 = function() {
     console.log(state);
 
+    //all of these individual statements connects the js variable
+    //to the ID in the HTML document
     const id = document.getElementById('character-id3');
     const name = document.getElementById('character-name3');
     const status = document.getElementById('character-status3');
@@ -173,6 +270,8 @@ let updateCard3 = function() {
     const origin = document.getElementById('character-origin3');
     const img = document.querySelector('#character-img3>img');
 
+    //each line changes the inner text of the ID's from above into
+    //the data from the api
     id.innerText = state.id;
     name.innerText = state.name;
     status.innerText = state.status;
@@ -182,28 +281,55 @@ let updateCard3 = function() {
     img.src = state.image;
 }
 
+//This is my async function that calls the api using fetch
+//This api call only calls one random character to load
+//into my character-stat-card 1 on bottom right of the html page.
 let getCharacterUsingAsync4 = async function(){
+
+    //Call of the random page
     randApiUrlPage();
+
+    //try/catch for the api Url
     try{
+        //fetching data with the api Url generated with randApiUrlPage
         const response = await fetch(apiUrl);
+        //awaiting the api data and storing the json request in js variable data
         const data = await response.json();
+         // the data from json is storred in an array
+        //index uses a formula to choose a random character from the array
+        //organized by id
         let index = Math.floor(Math.random()*data.results.length);
+
+         //some characters are not appropriate for the workplace so I've filtered these ID's
+        //its about 21 different ids
         const excludeArray = [98, 99, 140, 151, 153, 157, 192, 238, 244, 247, 271, 275, 309, 323, 342, 436, 459, 30, 41, 55, 97];
+         //My while loop to continue to choose a random ID until
+        //a workplace approved ID is chosen
         while (excludeArray.includes(data.results[index].id)){
             index = Math.floor(Math.random()*data.results.length);
             console.log(index);
         }
+
+        //after the loop, the character is stored in the state const from line 1
         state = data.results[index];
+        //the state is used in updateCard4 function
         updateCard4();
+
+        //in case there is an error
     } catch (err) {
         console.log('Error occured');
         console.error(err.stack);
     }
 }
 
+//This function uses getCharacterUsingAsync4 to update my first character
+//card in the bottom right of the html page
+//4 in the id denotes the 4th character card
 let updateCard4 = function() {
     console.log(state);
 
+    //all of these individual statements connects the js variable
+    //to the ID in the HTML document
     const id = document.getElementById('character-id4');
     const name = document.getElementById('character-name4');
     const status = document.getElementById('character-status4');
@@ -212,6 +338,8 @@ let updateCard4 = function() {
     const origin = document.getElementById('character-origin4');
     const img = document.querySelector('#character-img4>img');
 
+    //each line changes the inner text of the ID's from above into
+    //the data from the api
     id.innerText = state.id;
     name.innerText = state.name;
     status.innerText = state.status;
@@ -221,6 +349,8 @@ let updateCard4 = function() {
     img.src = state.image;
 }
 
+//Each of line between 352 and 360 adds an event listener 
+//in the HTML next to the button with the ID more-characters
 document.getElementById('more-characters')
         .addEventListener('click', getCharacterUsingAsync1);
 document.getElementById('more-characters')
@@ -230,7 +360,7 @@ document.getElementById('more-characters')
 document.getElementById('more-characters')
         .addEventListener('click', getCharacterUsingAsync4);
 
-
+//calls the function getCharacterUsingAsync for all of the cards
 getCharacterUsingAsync1();
 getCharacterUsingAsync2();
 getCharacterUsingAsync3();
--- Rick & Morty Project/search.js
+++ Rick & Morty Project/search.js
@@ -1,41 +1,54 @@
-// let state = {
-//     id: '',
-//     name: '',
-//     status: '',
-//     species: '',
-//     gender: '',
-//     origin: '',
-//     image: '',
-// };
 
-
+//SearchApiUrl is my function for using the value from inputed from
+//search-input and appending is the end of my Api Url using temporate
+//literals
 let searchApiUrl = function () {
 
+    //gets the value from the input submitted
     const searchValue = document.getElementById('search-input').value;
     
+    //Api requested using the filter /?name=
     let apiUrl= (`https://rickandmortyapi.com/api/character/?name=${searchValue}`);
 
+    //returns the apiUrl outside the searchApiUrl
     return apiUrl;
 }
 
+    //This is my async function for the fetch call to the Api
 let getCharacterUsingAsync = async function(){
 
+    //declares apiUrl inside this function
+    //Also calls for the function searchApiUrl
     const apiUrl = searchApiUrl();
 
     try{
+        //fetching data with the api Url generated with searchApiUrl
         const response = await fetch(apiUrl);
+        //awaiting the api data and storing the json request in js variable data
         const data = await response.json();
+
+        //calls function searchResult
+        //searchResult uses the data from the array
         searchResult(data.results[0]);
 
+        //used in case of an error
+        //very possible due to the fact that my search input will not 
+        //pull characters from misspelled input
+        //will not pull characters that do not exist
     } catch (err) {
         console.log('Error occured');
         console.error(err.stack);
         let error = document.getElementById('search-result-section');
+        //This message below shows in search-result-section when
+        //a character cannot be found using the name filter
         error.innerHTML = "<h4> Cannot Find Character </h4>";
     }
 }
 
+//called in getCharacterUsingAsync
+//stores response from api in an array inside results
 let searchResult = function (results) {
+    //state stores information from array into 7 different fields
     const state = {
         id: results.id,
         name: results.name,
@@ -46,8 +59,18 @@ let searchResult = function (results) {
         image: results.image,
     };
 
+    //CharacterSheet places the temporate literal HTML into the div with the
+    //ID search-result-section
+    //The block below is only created when a search is made.
+    //Each block replaces the previous block with every search
+    //temporate literals display information from state into character card
     let characterSheet = document.getElementById('search-result-section');
-    characterSheet.innerHTML = `
+    characterSheet.innerHTML = 
+    //I used innerHTML instead of create element because it was easier, but mitch let
+    //me know that this method is actually a security risk and it's not often used.
+    //Next time, I will use create element
+    //This character block is the same from randomizer html page
+    `
     
     <section class="block">
 
